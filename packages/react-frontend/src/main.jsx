906691ac995fd368daa32bdc58c19af734b78feb
ino464 <jonahqdepaz@gmail.com>
2026-09-02 13:45:43 -0700

    added npm start function

--- packages/react-frontend/src/main.jsx
+++ packages/react-frontend/src/main.jsx
@@ -1,7 +1,7 @@
+// src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import App from './MyApp.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
