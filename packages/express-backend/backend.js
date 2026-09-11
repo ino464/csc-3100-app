d2e561ee99cef345e8c32ef745e00dca81029dec
ino464 <jonahqdepaz@gmail.com>
2026-09-10 23:05:57 -0700

    commit

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,17 +4,6 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  app.get("/users", (req, res) => {
-  res.send(users);
-});
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
 
 const users = {
   users_list: [
@@ -45,3 +34,29 @@ const users = {
     },
   ],
 };
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
